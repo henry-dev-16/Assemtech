1a7f31f32e0a2405f0c7f7feef994f2cb7d0008b
henry-dev-16 <henrriortiz16@gmail.com>
2022-05-29 19:26:42 -0400

    resolvi algunas cosas

--- for-sale/code.js
+++ for-sale/code.js
@@ -1,4 +1,4 @@
-function create_Card(a,imagen,nombre,cualidad_1,cualidad_2, cualidad_3, cualidad_4){
+function create_Card(a,imagen,nombre,cualidad_1,cualidad_2, cualidad_3, cualidad_4, price){
     // Llamo el elemento para clonarlo y el padre para ingresar el clon
     let c = document.getElementById("card-1");
     let d = document.getElementById("card-container");
@@ -25,46 +25,83 @@ function create_Card(a,imagen,nombre,cualidad_1,cualidad_2, cualidad_3, cualidad
 
     let c2 = document.querySelectorAll("."+a+" > div > ul .li_2 ");
     c2 = c2.item(0);
-    console.log(c2)
     c_a = "<b>"+cualidad_2[0]+"</b>"+cualidad_2[1];
     c2.innerHTML=c_a;
 
     let c3 = document.querySelectorAll("."+a+" > div > ul .li_3 ");
     c3 = c3.item(0);
-    console.log(c3)
     c_a = "<b>"+cualidad_3[0]+"</b>"+cualidad_3[1];
     c3.innerHTML=c_a;
 
     let c4 = document.querySelectorAll("."+a+" > div > ul .li_4 ");
-    c4 = c4.item(0);
-    console.log(c4)
     c_a = "<b>"+cualidad_4[0]+"</b>"+cualidad_4[1];
     c4.innerHTML=c_a;
+
+    let pric = document.querySelectorAll("."+a+" > div > .btn-productos > h3")
+    pric = pric.item(0);
+    pric.innerHTML = "$"+ price;
+
+    
+    
+    
 
 }
 
 
 create_Card("card-2",
 "./assets/logo-AssemTech1.png",
-"ok diego ta feo",
-["color:","negro"],
-["sabor:","pupu"],
-["brocolee:","chaquichan"],
-["ajonjolee:","bruslee"]);
+"Intel Pentium 4",
+["Modelo:","SL6S2 MALAY"],
+["Frecuencia:","2.53ghz"],
+["Cache:","512kb"],
+["Núcleos:","1"],"35.00");
 
 create_Card("card-3",
 "./assets/logo-AssemTech1.png",
-"ok diego ta feo",
-["modelo:","tu-real-mm"],
-["marca:","generica"],
-["compatible:","motherboard-del"],
-["ajonjolee:","bruslee"]);
+"Intel Pentium Dual-Core",
+["Modelo:","SLAT7 MALAY"],
+["Frecuencia:","2.52ghz"],
+["Cache:","800kb"],
+["Núcleos:","2"],"35.00");
 
-create_Card("card-4",
+
+create_Card("card-2",
 "./assets/logo-AssemTech1.png",
-"ok diego ta feo",
-["color:","negro"],
-["sabor:","pupu"],
-["brocolee:","chaquichan"],
-["ajonjolee:","bruslee"]);
+"Intel Pentium 4",
+["Modelo:","SL6S2 MALAY"],
+["Frecuencia:","2.53ghz"],
+["Cache:","512kb"],
+["Núcleos:","1"],"35.00");
+
+
+create_Card("card-2",
+"./assets/logo-AssemTech1.png",
+"Intel Pentium 4",
+["Modelo:","SL6S2 MALAY"],
+["Frecuencia:","2.53ghz"],
+["Cache:","512kb"],
+["Núcleos:","1"],"35.00");
+
+
+create_Card("card-2",
+"./assets/logo-AssemTech1.png",
+"Intel Pentium 4",
+["Modelo:","SL6S2 MALAY"],
+["Frecuencia:","2.53ghz"],
+["Cache:","512kb"],
+["Núcleos:","1"],"35.00");
+
+
+create_Card("card-2",
+"./assets/logo-AssemTech1.png",
+"Intel Pentium 4",
+["Modelo:","SL6S2 MALAY"],
+["Frecuencia:","2.53ghz"],
+["Cache:","512kb"],
+["Núcleos:","1"],"35.00");
+
+
+
+
+
 
